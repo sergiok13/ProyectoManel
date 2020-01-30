358559b7ada06a28e65457bcd495bf64578e7c04
EQUIPO\Glorieta <gloriasedo4@gmail.com>
2020-01-30 19:09:53 +0100

    subo login

--- screens/login.js
+++ screens/login.js
@@ -12,18 +12,39 @@ export default class Login extends Component{
     constructor(props){
         super(props)
         this.state={
-            email: '',
+            correo: '',
             password: '',
+            resultadoFetch: []
         }
     }
 
-    handleEmail = (text) => {
-        this.setState({ email: text })
-     }
+    validar_email = (email) => {
+        var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (regex.test(email)) {
+            this.setState({ correo: email });
+            console.log("correo valido")
+        } else {
+            this.setState({ correo: "" })
+            console.log("error");
+        }
+    }
 
-     handlePassword = (text) => {
-        this.setState({ password: text })
-     }
+     guardarPassword = (text) => {
+        (text.length > 1)
+            ? this.setState({ password: text })
+            : this.setState({ password: "" })
+    }
+
+    
+
+    comprobarUsuario = async () => {
+        await this.iniciarSesion();
+        (this.state.resultadoFetch.length !== 0) ? this.props.navigation.navigate('Inicio') : alert("El correo o la contraseña no son correctos");;
+    }
+
+    pantallaRegistro = () => {
+        this.props.navigation.navigate('Register');
+    }
 
     render(){
         return(
@@ -32,25 +53,28 @@ export default class Login extends Component{
 
                 <Text>Correo Usuario</Text>
                 <TextInput
+                style={{ borderWidth: 1 }}
                 placeholder = "Correo"
-                onChangeText = {this.handleEmail}
+                onChangeText = {this.validar_email}
+                keyboardType='email-address'
                 />
 
                 <Text>Contraseña</Text>
                 <TextInput
+                style={{ borderWidth: 1 }}
                 placeholder = "Contraseña"
-                onChangeText = {this.handlePassword}
+                onChangeText = {this.guardarPassword}
                 />
 
                 <Button
                     title="Iniciar sesión"
-                    onPress={() => Alert.alert('Te vas a la pagina principal')}
+                    onPress={this.comprobarUsuario}
                     color="rgba(16,110,242,1)"
                 />
 
                 <Button
                     title="Crear cuenta"
-                    onPress={() => Alert.alert('Te vas al formulario crear cuenta')}
+                    onPress={this.pantallaRegistro}
                     color="rgb(16, 33, 117)"
                 />
             </View>
