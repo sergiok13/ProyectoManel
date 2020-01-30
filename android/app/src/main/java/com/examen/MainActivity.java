26ec7898c8bba5a4f79e557326d545d549429ce6
Sergio <sergio.qo000@gmail.com>
2020-01-30 15:01:54 +0100

    modifico gitignore y lo subo

---
+++ android/app/src/main/java/com/examen/MainActivity.java
@@ -0,0 +1,33 @@
+package com.examen;
+
+import com.facebook.react.ReactActivity;
+
+//Añado las tres siguientes lineas
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "examen";
+  }
+
+  //Añado este bloque de códgio
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+  //Hasta aqui
+
+}
